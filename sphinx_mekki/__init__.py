6e4a2d1a55086fe0f632cda3f484120395665c22
Yoshiyuki Wakinotani <wakinotani@gmail.com>
2024-10-20 00:57:19 +0900

    Set version to 0.8.2

--- sphinx_mekki/__init__.py
+++ sphinx_mekki/__init__.py
@@ -21,7 +21,7 @@ from sphinx.writers.html import HTML5Translator
 from sphinx.environment import BuildEnvironment
 from sphinx.environment.collectors.toctree import TocTreeCollector
 
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 
 logger = logging.getLogger(__name__)
 
