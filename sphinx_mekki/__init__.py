c130acbe13ba426565ac9812cce6abb6411814de
Yoshiyuki Wakinotani <wakinotani@gmail.com>
2025-01-12 22:30:26 +0900

    Set the version to 0.8.5.

--- sphinx_mekki/__init__.py
+++ sphinx_mekki/__init__.py
@@ -23,7 +23,7 @@ from sphinx.environment import BuildEnvironment
 from sphinx.environment.collectors.toctree import TocTreeCollector
 from sphinx.util.images import get_image_size
 
-__version__ = "0.8.4"
+__version__ = "0.8.5"
 
 logger = logging.getLogger(__name__)
 
